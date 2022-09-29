e66689770f2d84e91c4ec3f9938a6890e045ef05
uhzza <whdgns955@naver.com>
2022-09-29 10:46:02 +0900

    이종훈 -pom에 로그4jdbc 추가햇음, 맵퍼 추가,디에이오작업

---
+++ project/src/test/java/com/example/demo/SstoreDaoTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.catalina.Store;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.seller.dao.SstoreDao;
+import com.example.demo.seller.entity.Sstore;
+
+@SpringBootTest
+public class SstoreDaoTest {
+
+	@Autowired
+	SstoreDao sStoreDao;
+	
+	@Transactional
+	@Test
+	public void StoreAddTest() {
+		Sstore store = Sstore.builder().sStoreName("김밥천국").sStoreAddress("학익동3-1")
+				.sMinDeleVery(20000).sStoreIntro("종합분식 김밥천국입니").build();
+		assertEquals(1, sStoreDao.StoreAdd(store));
+	}
+	
+}
