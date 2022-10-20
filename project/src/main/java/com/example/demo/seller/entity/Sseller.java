0941461bda16a464d10084aa4a03af84237263f5
준엽 <cieyuk@gmail.com>
2022-10-20 09:06:15 +0900

    준엽 - entity

---
+++ project/src/main/java/com/example/demo/seller/entity/Sseller.java
@@ -0,0 +1,46 @@
+package com.example.demo.seller.entity;
+
+import java.time.*;
+
+import com.example.demo.seller.dto.SsellerDto;
+import com.example.demo.seller.dto.SsellerDto.Read;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Sseller {
+	private String sId;
+	private Integer sBusinessNum;
+	private String sPassword;
+	private String sName;
+	private LocalDate sBirth;
+	private Integer sPhone;
+	private String sEmail;
+	private Integer sStoreNum;
+	private LocalDate sJoinday;
+	private String sRole;
+	private Integer sLoginFailCnt;
+	private Boolean sEnabled;
+	private String sCheckcode;
+	private sLevel sLevel;
+
+	public void sAddJoinInfo(String checkcode, String encodePassword, sLevel sLevels) {
+		this.sCheckcode = checkcode;
+		this.sPassword = encodePassword;
+		this.sLevel = sLevels;
+	}
+
+	public Read toDto() {
+		return SsellerDto.Read.builder()
+				.sId(sId).sName(sName)
+				.sEmail(sEmail)
+				.sBirth(sBirth)
+				.sJoinday(sJoinday)
+				.slevel(sLevel)
+				.build();
+	}
+}
